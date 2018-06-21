78a26d66f42515d1a0865ee3300bece30bc12803
Shi Mei Wong <wshimei@mgmail.com>
2018-06-21 22:22:20 +0800

    updated to angular 6

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,12 @@
+
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+
+import {catchError} from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { IEmployee } from './employee';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/catch';
-import 'rxjs/add/observable/throw'
+
+
 
 @Injectable()
 export class EmployeeService {
@@ -13,11 +16,11 @@ export class EmployeeService {
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<IEmployee[]> {
-    return this.http.get<IEmployee[]>(this._url)
-                    .catch(this.errorHandler);
+    return this.http.get<IEmployee[]>(this._url).pipe(
+                    catchError(this.errorHandler));
   }
 
   errorHandler(error: HttpErrorResponse){
-    return Observable.throw(error.message || "Server Error")
+    return observableThrowError(error.message || "Server Error")
   }
 }
